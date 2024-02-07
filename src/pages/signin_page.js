4a3ea1c20aaec95200674df824cd7085f2b7e1a9
KKsurendran06 <kksurendran95@gmail.com>
2024-02-07 20:55:09 +0530

    style: restructure the code

---
+++ src/pages/signin_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../style/signin_page.css";
+import Navbar from "../components/navbar";
+
+
+const Signin = () => {
+    return (
+        <Navbar/>
+
+    )
+
+}
+
+export default Signin;
